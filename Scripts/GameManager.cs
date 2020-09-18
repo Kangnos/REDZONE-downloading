79c35a4e1d238fc0653dc1a258fa9cd9003f33b1
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-09-18 23:05:59 +0900

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public GameObject[] Bombs;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    public Slider HealthSlider;
+    int randBomb;
+    public Text countnumber;
+    private float time;
+    private int StartTime;
+    public GameObject WinText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+        time = 60f;
+        time = time - Time.deltaTime;
+        countnumber.text = Mathf.Ceil(time).ToString();
+
+        if (time <= 1) {
+            gameObject.SetActive(false);
+
+            WinText.SetActive(true);
+        }
+
+
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+
+            randBomb = Random.Range(0, 2);
+
+
+
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 4, Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(Bombs[randBomb], spawnPos + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 20f;
+
+    private Rigidbody rb;
+
+    public float shiftSpeed = 17f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = shiftSpeed;
+        }
+        else{
+            speed = 20f;
+        }
+
+        if (Input.GetKey(KeyCode.F)) {
+            speed = 10f;
+        }
+    }
+}
